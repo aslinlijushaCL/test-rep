74a94a01c8ed12a481384e868bc789b1605aa8a1
Aslin <aslin@cloudliongroup.com>
2026-06-01 16:37:29 +0530

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# {
+#     'name': 'Custom Sale Approval',
+#     'version': '1.0',
+#     'summary': 'Create invoice from Sales easily',
+#     'depends': ['base', 'sale', 'account'],
+#     'data': [
+#         'security/ir.model.access.csv',
+#         'views/views.xml',
+#     ],
+#     'installable': True,
+#     'application': False,
+# }
+# {
+#     'name': 'Custom Sales Approval',
+#     'version': '1.0',
+#     'depends': ['sale'],
+#     'data': [
+#         'views/views.xml',
+#     ],
+#     'installable': True,
+# }
+{
+    'name': 'Test Module',
+    'version': '1.0',
+    'depends': ['base'],
+    'installable': True,
+    'license': 'LGPL-3',
+    'author': 'Cl',
+
+}
